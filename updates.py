4370f9da4f9313173138ee7867301eaf24f53bcd
Joshua Banjo <wun2393@gmail.com>
2025-10-25 23:54:00 +0100

    still working

--- updates.py
+++ updates.py
@@ -1,2 +1,3 @@
 print("Completed first lesson in linux series on boot.dev - 22/10/25")
 print("Will this register?")
+print("Currently working on product market research - nothing major to push today")
